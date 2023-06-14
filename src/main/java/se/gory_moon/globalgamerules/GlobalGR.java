b35ff3967f1c80afc22b3777c14fb25f71c73d06
Gustaf Järgren <gurreja@gmail.com>
2023-06-14 13:52:48 +0200

    Fix crashing on world load and not creating config (#15)
    * fix: fixed crashing from other mods gamerules
    
    * fix: require minimum 1.19.2 and fix command issue

--- src/main/java/se/gory_moon/globalgamerules/GlobalGR.java
+++ src/main/java/se/gory_moon/globalgamerules/GlobalGR.java
@@ -7,12 +7,16 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.event.lifecycle.FMLConstructModEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 /**
  * The main mod class used for config setup and event-listener registering
  */
 @Mod(GlobalGR.MOD_ID)
 public class GlobalGR {
+
+    private static final Logger LOGGER = LogManager.getLogger("GlobalGameRules");
 
     /**
      * The mod id
@@ -29,6 +33,12 @@ public class GlobalGR {
 
     private void constructMod(FMLConstructModEvent event) {
         ModContainer container = ModLoadingContext.get().getActiveContainer();
-        event.enqueueWork(() -> container.addConfig(new ModConfig(ModConfig.Type.COMMON, GGRConfig.commonSpec, container)));
+        event.enqueueWork(() -> {
+            try {
+                container.addConfig(new ModConfig(ModConfig.Type.COMMON, Config.commonSpec, container));
+            } catch (Throwable e) {
+                LOGGER.error("Error creating config", e);
+            }
+        });
     }
 }
