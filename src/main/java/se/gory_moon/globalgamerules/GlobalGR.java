b0bb25b72c0842ab80439861dac4ecc4b1fce545
GoryMoon <gurreja@gmail.com>
2025-03-29 09:17:06 +0100

    fix: fixed hardcode setting always being enforced

--- src/main/java/se/gory_moon/globalgamerules/GlobalGR.java
+++ src/main/java/se/gory_moon/globalgamerules/GlobalGR.java
@@ -1,11 +1,8 @@
 package se.gory_moon.globalgamerules;
 
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.fml.ModContainer;
-import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
-import net.minecraftforge.fml.event.lifecycle.FMLConstructModEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -26,19 +23,9 @@ public class GlobalGR {
     /**
      * Registers the construction method for the config and registers the event-listener for the world events
      */
-    public GlobalGR() {
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::constructMod);
+    public GlobalGR(FMLJavaModLoadingContext context) {
         MinecraftForge.EVENT_BUS.register(WorldEvents.class);
-    }
 
-    private void constructMod(FMLConstructModEvent event) {
-        ModContainer container = ModLoadingContext.get().getActiveContainer();
-        event.enqueueWork(() -> {
-            try {
-                container.addConfig(new ModConfig(ModConfig.Type.COMMON, Config.commonSpec, container));
-            } catch (Throwable e) {
-                LOGGER.error("Error creating config", e);
-            }
-        });
+        context.getContainer().addConfig(new ModConfig(ModConfig.Type.COMMON, Config.commonSpec, context.getContainer()));
     }
 }
